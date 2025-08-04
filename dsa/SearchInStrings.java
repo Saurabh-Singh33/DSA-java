490b09bd26330494de9d7345baab4b9c508a29ef
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2025-08-04 22:45:37 +0530

    more files

--- dsa/SearchInStrings.java
+++ dsa/SearchInStrings.java
@@ -1,39 +1,29 @@
 package dsa;
-import java.util.Scanner;
+
+import java.util.Arrays;
+
 public class SearchInStrings {
-
-
-
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-
-            // Input main string
-            System.out.print("Enter the string: ");
-            String str = sc.nextLine();
-
-            // Input the target character
-            System.out.print("Enter the character to search: ");
-            char target = sc.next().charAt(0);
-
-            // Perform linear search
-            int index = searchChar(str, target);
-
-            if (index != -1) {
-                System.out.println("Character found at index: " + index);
-            } else {
-                System.out.println("Character not found.");
-            }
-
-            sc.close();
-        }
-
-        // Linear search for a character
-        static int searchChar(String str, char target) {
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == target) {
-                    return i;
-                }
-            }
-            return -1;
-        }
+    public static void main(String[] args) {
+        String name = "Saurabh";
+        char target = 'b';
+        System.out.println(search(name, target)); // should print true
+        System.out.println(Arrays.toString(name.toCharArray()));
     }
+
+    static boolean search(String str, char target) {
+        if (str.length() == 0) {
+            return false;
+        }
+
+        for (char ch : str.toCharArray()) {
+            if (ch == target) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+
+
