cfe03c885bef4120471f7653f163868d0e116841
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2025-08-06 05:09:02 +0530

    leecode DSA

--- dsa/EvenDigits.java
+++ dsa/EvenDigits.java
@@ -2,48 +2,47 @@ package dsa;
 
 public class EvenDigits {
     public static void main(String[] args) {
-           int [] nums = {12,345,2,6,7896};
-           System.out.println(FindNumbers(nums));
-           System.out.println(digits(-23462));
+        int [] nums = {12,345,2,6,7896};
+        System.out.println(FindNumber(nums));
+        System.out.println(digits(2387569));
     }
-    static int FindNumbers(int [] nums){
+    static int FindNumber(int [] nums){
         int count = 0;
-        for(int num : nums){
+        for (int num : nums){
             if(even(num)){
                 count ++;
-            }
 
+            }
         }
         return count;
     }
-//function to check weather number contains even digit or not
+// To check  it is even or not
      static boolean even(int num) {
-      int NumberOfDigits = digits(num);
+        int NumberOfDigits = digits(num);
+     /*   if (NumberOfDigits % 2 ==0){
+            return true ;
+        }
+        return false;*/
+         return NumberOfDigits % 2 ==0;
+    }
 
-
-             if (NumberOfDigits % 2 == 0) {
-                 return true;
-             }
-             return false;
-             // return numberOfDigits % 2 ==0;
-
-
-         }
-
-    //count number of digits in number
+    // for further optimization
+    /* static int digits(int num){
+    return (int(Math.log10(num)) + 1;
+     */
     static int digits(int num){
-        if (num ==0) {
+        if(num == 0){
             return 1;
         }
-        if (num < 0){
+        if (num <0 ){
             num = num * -1;
         }
         int count = 0;
-        while(num > 0){
-            count ++;
-            num=num/10;  // num /=10
-        }
-        return count;
 
-}
+        while(num > 0){
+            count++;
+            num = num/10;
+        }
+        return count ;
+    }
 }
